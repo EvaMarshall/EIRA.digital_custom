46eb293ba05ab74aeeb42ff9c8cb9c724b4af25d
Eva Marshall <148203160+EvaMarshall@users.noreply.github.com>
2026-05-05 12:43:37 +0200

    google searhc console and sitemep

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-  ],
+    site: "https://eira.digital",
+  integrations: [tailwind(), react(), sitemap()],
 });
